52e06f19edfb47c8eeb631be91ee853bdf255238
2ndwav-okubo <ookubo@2ndwave.jp>
2021-06-23 11:20:51 +0900

    検索欄の全表示修正

--- app/controllers/blogs/searches_controller.rb
+++ app/controllers/blogs/searches_controller.rb
@@ -1,13 +1,15 @@
 class Blogs::SearchesController < ApplicationController
   def index
     @blogs = Blog.where('title LIKE(?)', "%#{params[:title]}%")
-    
-    respond_to do |format|
-      format.html { redirect_to :root }
+    @blogs_value = "%#{params[:title]}%"
+    if @blogs_value == "%%"
+    else 
+      respond_to do |format|
+        format.html { redirect_to :root }
 
-      # ↓検索結果のデータをレスポンスするコード
-        format.json { render json: @blogs }
-    
-  end
+        # ↓検索結果のデータをレスポンスするコード
+        format.json { render json: @blogs}
+      end
+    end  
   end
 end
